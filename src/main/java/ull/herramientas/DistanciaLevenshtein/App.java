add633ecc679e36dffbb7ba1197cccf0b983289c
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-12-04 13:36:24 +0000

    creación proyecto

---
+++ src/main/java/ull/herramientas/DistanciaLevenshtein/App.java
@@ -0,0 +1,13 @@
+package ull.herramientas.DistanciaLevenshtein;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Hola mundo");
+    }
+}
